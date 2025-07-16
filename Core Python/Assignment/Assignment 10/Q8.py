8d7480aed6745ffbae24ccc0ee7d55783d5220cd
Sankalp Nighut <sankalpnighut12@gmail.com>
2025-07-16 19:19:23 +0530

    Add files via upload

---
+++ Core Python/Assignment/Assignment 10/Q8.py
@@ -0,0 +1,8 @@
+original_list = [10, 20, 30, 40, 50]
+
+duplicate_list = original_list.copy()
+
+duplicate_list[0] = 999
+
+print("Original List:", original_list)
+print("Duplicate List:", duplicate_list)
